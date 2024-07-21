b802b8c6ff553c9d0a5cda3108dad865ff0b7778
Gaab shiine <gaabshiine@gmail.com>
2024-07-21 07:51:12 +0300

    error checking

--- home_page_app/urls.py
+++ home_page_app/urls.py
@@ -7,4 +7,16 @@ app_name = "home_page_app"
 
 urlpatterns = [
     path("", views.home_view, name="home"),
+
+    path("about/", views.about_view, name="about"),
+
+    path("contact/", views.contact_view, name="contact"),
+
+    path("membership/", views.membership_view, name="membership"),
+
+    path("faqs/", views.faqs_view, name="faqs"),
+
+    # event urls
+    path("event_list/", views.event_list_view, name="event_list"),
+    path("event_detail/<int:id>/", views.event_detail_view, name="event_detail"),
 ]
--- home_page_app/views.py
+++ home_page_app/views.py
@@ -4,3 +4,27 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, "home_page_app/index.html")
+
+
+def about_view(request):
+    return render(request, "home_page_app/about.html")
+
+
+def contact_view(request):
+    return render(request, "home_page_app/contact.html")
+
+
+def membership_view(request):
+    return render(request, "home_page_app/membership_plans.html")
+
+
+def faqs_view(request):
+    return render(request, "home_page_app/FAQs.html")
+
+
+def event_list_view(request):
+    return render(request, "home_page_app/event_list.html")
+
+def event_detail_view(request, id):
+    return render(request, "home_page_app/event_detail.html")
+
